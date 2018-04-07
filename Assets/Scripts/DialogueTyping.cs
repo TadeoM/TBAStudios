eed8586468cecb33fa0f0e320af732af94855f81
Jeb Atkinson <jebatkin681@gmail.com>
2018-04-07 16:05:30 -0400

    Dialogue minigame is almost there

---
+++ Assets/Scripts/DialogueTyping.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTyping : MonoBehaviour {
+    //attributes
+    string word;
+    int charPos;
+
+    public Canvas canvasParent;
+
+    public Text charBlack;
+    public Text charWhite;
+
+    float spacing;
+
+    Vector3 posBlack;
+    Vector3 posWhite;
+
+    List<Text> charsBlack;
+    List<Text> charsWhite;
+
+    List<string[]> dialogueFiles = new List<string[]>();
+
+    int dialogueFile;
+
+    string[] lines;
+
+    int line;
+
+    public Text npcText;
+
+    string[] npcLines = System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\npcTest.txt");
+
+    int npcLine;
+
+    // Use this for initialization
+    void Start() {
+
+        dialogueFiles.Add(System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Greeting1.txt"));
+        dialogueFiles.Add(System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Goodbye1.txt"));
+
+        dialogueFile = 0;
+
+        lines = dialogueFiles[dialogueFile];
+
+        line = 0;
+
+        spacing = 25;
+
+        charPos = 0;
+
+        word = lines[line];
+
+        charsBlack = new List<Text>();
+        charsWhite = new List<Text>();
+
+        posBlack = charBlack.transform.position;
+        posWhite = charWhite.transform.position;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            posBlack.x += spacing;
+            charsBlack.Add(Instantiate(charBlack, posBlack, Quaternion.identity));
+            charsBlack[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            posWhite.x += spacing;
+            charsWhite.Add(Instantiate(charWhite, posWhite, Quaternion.identity));
+            charsWhite[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            charsBlack[i].text = word.Substring(i, 1);
+        }
+
+        npcLine = 0;
+
+        npcText.text = npcLines[npcLine];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (charPos < word.Length)
+        {
+            if (Input.GetKeyDown(word.Substring(charPos, 1)))
+            {
+                charsWhite[charPos].text = word.Substring(charPos, 1);
+                Destroy(charsBlack[charPos]);
+                charPos++;
+            }
+        }
+
+        if (charPos == word.Length)
+        {
+            charPos = 0;
+            line++;
+
+            if(line < lines.Length)
+            {
+                CharArrayReset();
+            }
+        }
+
+        if(line == lines.Length)
+        {
+            line = 0;
+
+            dialogueFile++;
+
+            if (dialogueFile < dialogueFiles.Count)
+            {
+                lines = dialogueFiles[dialogueFile];
+            }
+
+            if(dialogueFile != dialogueFiles.Count)
+            {
+                CharArrayReset();
+            }
+            npcLine++;
+
+            if(npcLine < npcLines.Length)
+            {
+                npcText.text = npcLines[npcLine];
+            }
+        }
+    }
+
+    void CharArrayReset()
+    {
+        word = lines[line];
+
+        for (int i = 0; i < charsWhite.Count; i++)
+        {
+            Destroy(charsWhite[i]);
+        }
+
+        charsBlack = new List<Text>();
+        charsWhite = new List<Text>();
+
+        posBlack = charBlack.transform.position;
+        posWhite = charWhite.transform.position;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            posBlack.x += spacing;
+            charsBlack.Add(Instantiate(charBlack, posBlack, Quaternion.identity));
+            charsBlack[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            posWhite.x += spacing;
+            charsWhite.Add(Instantiate(charWhite, posWhite, Quaternion.identity));
+            charsWhite[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            charsBlack[i].text = word.Substring(i, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/TextJiggle.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextJiggle : MonoBehaviour {
+
+    bool vertMove;
+    bool horizMove;
+
+    int vertTime;
+    int horizTime;
+
+    int randScale;
+    int scale;
+
+    int randVert;
+    int randHoriz;
+
+	// Use this for initialization
+	void Start () {
+        vertMove = true;
+        horizMove = true;
+
+        vertTime = 0;
+        horizTime = 0;
+
+        int randScale = Random.Range(0, 1);
+
+        if(randScale == 0)
+        {
+            scale = 4;
+        }
+        else
+        {
+            scale = -4;
+        }
+
+        randVert = Random.Range(15, 45);
+        randHoriz = Random.Range(30, 60);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (vertTime > randVert)
+        {
+            if (vertMove)
+            {
+                transform.Translate(Vector3.up * scale);
+                vertMove = !vertMove;
+            }
+            else
+            {
+                transform.Translate(Vector3.down * scale);
+                vertMove = !vertMove;
+            }
+            randVert = Random.Range(15, 45);
+            vertTime = 0;
+        }
+
+        if(horizTime > randHoriz)
+        {
+            if (horizMove)
+            {
+                transform.Translate(Vector3.right * scale);
+                horizMove = !horizMove;
+            }
+            else
+            {
+                transform.Translate(Vector3.left * scale);
+                horizMove = !horizMove;
+            }
+            randHoriz = Random.Range(30, 60);
+            horizTime = 0;
+        }
+
+        vertTime++;
+        horizTime++;
+    }
+}
