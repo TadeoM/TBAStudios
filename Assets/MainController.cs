fa048d1b1f876dae7f05f05324cc211f181f868f
Gilexarr <tmenichel@gmail.com>
2018-04-07 10:14:49 -0400

    General Assets

---
+++ Assets/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] nonPlayers;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        FindNonPlayers();
+    } 
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+    }
+
+    /// <summary>
+    /// Find the NPC characters
+    /// </summary>
+    void FindNonPlayers()
+    {
+        nonPlayers = GameObject.FindGameObjectsWithTag("NPC");
+    }
+}
