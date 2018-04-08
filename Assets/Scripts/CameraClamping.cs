6dad1cea73847bcc63a0a7f64ec7f3d5ff6828d7
lestherreynoso <lhreynos@gmail.com>
2018-04-08 14:05:42 -0400

    Auto stash before merge of "developed" and "origin/developed"

--- Assets/Scripts/CameraClamping.cs
+++ Assets/Scripts/CameraClamping.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CameraClamping : MonoBehaviour {
 
-    public Vector2 minBounds;
-    public Vector2 maxBounds;
+    public Vector2 minBounds; // -9, -6.425
+    public Vector2 maxBounds; // 9 , 6.425
+
+    public float bottomOfApartment = -7.8f;
+    public float apartmentWidth = 11.5f;
+    // -7.8f bottom of the apartment
 
     // Use this for initialization
     void Start () {
@@ -13,7 +17,14 @@ public class CameraClamping : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void LateUpdate () {
+        Vector3 position = transform.position;
+
+        float orthoWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+
+        position.x = Mathf.Clamp(position.x, -apartmentWidth + orthoWidth, apartmentWidth - orthoWidth);
+        position.y = Mathf.Clamp(position.y, Camera.main.orthographicSize + bottomOfApartment, 10);
+
+        transform.position = position;
+    }
 }
