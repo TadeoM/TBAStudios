243685699db2197b0f1df8b72ce0daff1c3fbccc
Gilexarr <tmenichel@gmail.com>
2018-04-07 22:49:06 -0400

    Dialogues done

--- Assets/Scripts/Dialogues.cs
+++ Assets/Scripts/Dialogues.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Dialogues : MonoBehaviour {
 
     public GameObject joeJones;
-    public GameObject sarah;
+    public GameObject elizabeth;
     public GameObject william;
     public GameObject spongegar;
     public bool isSetup;
@@ -24,7 +24,7 @@ public class Dialogues : MonoBehaviour {
     public void SetupEverything()
     {
         SetupJoeJones();
-        SetupSarah();
+        SetupElizabeth();
         SetupWilliam();
         SetupSpongegar();
         isSetup = true;
@@ -47,30 +47,34 @@ public class Dialogues : MonoBehaviour {
         joeJones.GetComponent<NPCInteractions>().NewDaySetup();
     }
 
-    public void SetupSarah()
+    public void SetupElizabeth()
     {
-        string[,] conversation = sarah.GetComponent<NPCInteractions>().Conversations;
-        conversation = new string[5, 5] 
+        string[,] conversation = elizabeth.GetComponent<NPCInteractions>().Conversations;
+        conversation = new string[1, 8] 
         { 
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" }
+            { "'Scuse me young man, have you seen my cat, My.Snugglesworth",
+                "Sorry, I haven't seen him/No, but what does it look like?",
+                "Oh Mr.Snugglesworth, where are you....",
+                "HE is light grey with grey feet and tail",
+                "Why did he run away?/I'll track him down!",
+                "Well he hasn't eaten in a dayy...I forgot to feed him/Oh! Thank you so much!",
+                "I'll help you find him, buut you can't stop feeding him/Maybe if you bring out food he'll come back.",
+                "You're right I'll be more particular next time. Thank you/Thank you, you're a doll!" }
         } ;
-        sarah.GetComponent<NPCInteractions>().Conversations = conversation;
-        sarah.GetComponent<NPCInteractions>().NewDaySetup();
+        elizabeth.GetComponent<NPCInteractions>().Conversations = conversation;
+        elizabeth.GetComponent<NPCInteractions>().NewDaySetup();
     }
 
     public void SetupWilliam()
     {
         string[,] conversation = william.GetComponent<NPCInteractions>().Conversations;
-        conversation = new string[5, 5]{
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" }
+        conversation = new string[1, 7]{
+            { "*munch munch*", "Wow./You should probably cook that before you eat it.",
+                ".../I don't know how to cook.",
+                "That is quite unhealthy, you should learn some recipes/I'm going to cook some food for us.",
+                "I don't have the time/Oh that sounds incredible.",
+                "Me too bud, but cooking your own food will give you energy/Let's cook some food together and you'll learn the recipe",
+                "I guess you're right, I'll try to learn some recipes/That sounds awesome! Let's do it." }
         };
         william.GetComponent<NPCInteractions>().Conversations = conversation;
         william.GetComponent<NPCInteractions>().NewDaySetup();
@@ -78,12 +82,13 @@ public class Dialogues : MonoBehaviour {
     public void SetupSpongegar()
     {
         string[,] conversation = spongegar.GetComponent<NPCInteractions>().Conversations;
-        conversation = new string[5, 5]{
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" },
-            { "one", "Two", "Three", "Four", "Five" }
+        conversation = new string[1, 7]{
+            { "...", ".../So..what are you doing?",
+                ".../I'm trying to think of what my next art piece should be.",
+                "Is there any way I can help?/I can give you some ideas",
+                "I usually have poems to help me inspire me/That might help.",
+                "I'm confident we can get some ideas/I've been told my poetry is really good",
+                "I'll put my faith in you/I hope you're right" },
         };
         spongegar.GetComponent<NPCInteractions>().Conversations = conversation;
         spongegar.GetComponent<NPCInteractions>().NewDaySetup();
