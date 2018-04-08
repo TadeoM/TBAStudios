18a97692416caab790486e2f22ea85ec5e3c397a
Jeb Atkinson <jebatkin681@gmail.com>
2018-04-07 23:52:37 -0400

    Need to destroy extra chars still

--- Assets/Scripts/DialogueTyping.cs
+++ Assets/Scripts/DialogueTyping.cs
@@ -5,47 +5,100 @@ using UnityEngine.UI;
 
 public class DialogueTyping : MonoBehaviour {
     //attributes
-    string word;
-    int charPos;
+    string wordSun;
+    string wordStar;
+    string wordBonfire;
+    string wordCandle;
+
+    int charPosSun;
+    int charPosStar;
+    int charPosBonfire;
+    int charPosCandle;
 
     public Canvas canvasParent;
 
-    public Text charBlack;
-    public Text charWhite;
+    public Text charBlackSun;
+    public Text charWhiteSun;
 
-    public Text poemLine1;
-    public Text poemLine2;
-    public Text poemLine3;
+    public Text charBlackStar;
+    public Text charWhiteStar;
+
+    public Text charBlackBonfire;
+    public Text charWhiteBonfire;
+
+    public Text charBlackCandle;
+    public Text charWhiteCandle;
+
+    public Text[] poem = new Text[3];
 
     float spacing;
 
-    Vector3 posBlack;
-    Vector3 posWhite;
+    Vector3 posBlackSun;
+    Vector3 posWhiteSun;
 
-    List<Text> charsBlack;
-    List<Text> charsWhite;
+    Vector3 posBlackStar;
+    Vector3 posWhiteStar;
 
-    string[] lines;
+    Vector3 posBlackBonfire;
+    Vector3 posWhiteBonfire;
+
+    Vector3 posBlackCandle;
+    Vector3 posWhiteCandle;
+
+    List<Text> charsBlackSun;
+    List<Text> charsWhiteSun;
+
+    List<Text> charsBlackStar;
+    List<Text> charsWhiteStar;
+
+    List<Text> charsBlackBonfire;
+    List<Text> charsWhiteBonfire;
+
+    List<Text> charsBlackCandle;
+    List<Text> charsWhiteCandle;
+
+    string[] linesSun;
+    string[] linesStar;
+    string[] linesBonfire;
+    string[] linesCandle;
 
     int line;
 
     // Use this for initialization
     void Start() {
-
-        lines = System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Sun.txt");
+        
+        linesSun = System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Sun.txt");
+        linesStar = System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Star.txt");
+        linesBonfire = System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Bonfire.txt");
+        linesCandle = System.IO.File.ReadAllLines(@"C:\Users\Jeb\Desktop\School\Jams\IThrive Kindness Text\Candle.txt");
 
         spacing = 25;
 
-        charPos = 0;
+        charPosSun = 0;
+        charPosStar = 0;
+        charPosBonfire = 0;
+        charPosCandle = 0;
 
-        word = lines[line];
-        
-        charsBlack = new List<Text>();
-        charsWhite = new List<Text>();
+        line = 0;
 
-        posBlack = charBlack.transform.position;
-        posWhite = charWhite.transform.position;
+        wordSun = linesSun[line];
+        wordStar = linesStar[line];
+        wordBonfire = linesBonfire[line];
+        wordCandle = linesCandle[line];
 
+        charsBlackSun = new List<Text>();
+        charsWhiteSun = new List<Text>();
+
+        charsBlackStar = new List<Text>();
+        charsWhiteStar = new List<Text>();
+
+        charsBlackBonfire = new List<Text>();
+        charsWhiteBonfire = new List<Text>();
+
+        charsBlackCandle = new List<Text>();
+        charsWhiteCandle = new List<Text>();
+
+        /*
         for (int i = 0; i < word.Length; i++)
         {
             posBlack.x += spacing;
@@ -64,11 +117,13 @@ public class DialogueTyping : MonoBehaviour {
         {
             charsBlack[i].text = word.Substring(i, 1);
         }
+        */
+        SetUp();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        /*
         if (charPos < word.Length)
         {
             if(word.Substring(charPos, 1) == " ")
@@ -86,9 +141,134 @@ public class DialogueTyping : MonoBehaviour {
         if (charPos == word.Length)
         {
             charPos = 0;
+
+            poem[line].text = lines[line];
+
             line++;
 
             if(line < lines.Length)
+            {
+                CharArrayReset();
+            }
+        }
+        */
+        GamePlay();
+    }
+
+    void GamePlay()
+    {
+        if (charPosSun < wordSun.Length)
+        {
+            if (wordSun.Substring(charPosSun, 1) == " ")
+            {
+                charPosSun++;
+            }
+            else if (Input.GetKeyDown(wordSun.Substring(charPosSun, 1)))
+            {
+                charsWhiteSun[charPosSun].text = wordSun.Substring(charPosSun, 1);
+                Destroy(charsBlackSun[charPosSun]);
+                charPosSun++;
+            }
+        }
+
+        if (charPosSun == wordSun.Length)
+        {
+            charPosSun = 0;
+
+            poem[line].text = linesSun[line];
+
+            line++;
+
+            if (line < linesSun.Length)
+            {
+                CharArrayReset();
+            }
+        }
+
+        //-------------------------------------------------
+
+        if (charPosStar < wordStar.Length)
+        {
+            if (wordStar.Substring(charPosStar, 1) == " ")
+            {
+                charPosStar++;
+            }
+            else if (Input.GetKeyDown(wordStar.Substring(charPosStar, 1)))
+            {
+                charsWhiteStar[charPosStar].text = wordStar.Substring(charPosStar, 1);
+                Destroy(charsBlackStar[charPosStar]);
+                charPosStar++;
+            }
+        }
+
+        if (charPosStar == wordStar.Length)
+        {
+            charPosStar = 0;
+
+            poem[line].text = linesStar[line];
+
+            line++;
+
+            if (line < linesStar.Length)
+            {
+                CharArrayReset();
+            }
+        }
+
+        //----------------------------------------------------------
+        if (charPosBonfire < wordBonfire.Length)
+        {
+            if (wordBonfire.Substring(charPosBonfire, 1) == " ")
+            {
+                charPosBonfire++;
+            }
+            else if (Input.GetKeyDown(wordBonfire.Substring(charPosBonfire, 1)))
+            {
+                charsWhiteBonfire[charPosBonfire].text = wordBonfire.Substring(charPosBonfire, 1);
+                Destroy(charsBlackBonfire[charPosBonfire]);
+                charPosBonfire++;
+            }
+        }
+
+        if (charPosBonfire == wordBonfire.Length)
+        {
+            charPosBonfire = 0;
+
+            poem[line].text = linesBonfire[line];
+
+            line++;
+
+            if (line < linesBonfire.Length)
+            {
+                CharArrayReset();
+            }
+        }
+
+        //--------------------------------------------------------
+
+        if (charPosCandle < wordCandle.Length)
+        {
+            if (wordCandle.Substring(charPosCandle, 1) == " ")
+            {
+                charPosCandle++;
+            }
+            else if (Input.GetKeyDown(wordCandle.Substring(charPosCandle, 1)))
+            {
+                charsWhiteCandle[charPosCandle].text = wordCandle.Substring(charPosCandle, 1);
+                Destroy(charsBlackCandle[charPosCandle]);
+                charPosCandle++;
+            }
+        }
+
+        if (charPosCandle == wordCandle.Length)
+        {
+            charPosCandle = 0;
+
+            poem[line].text = linesCandle[line];
+
+            line++;
+
+            if (line < linesCandle.Length)
             {
                 CharArrayReset();
             }
@@ -97,6 +277,7 @@ public class DialogueTyping : MonoBehaviour {
 
     void CharArrayReset()
     {
+        /*
         word = lines[line];
 
         for (int i = 0; i < charsWhite.Count; i++)
@@ -127,6 +308,238 @@ public class DialogueTyping : MonoBehaviour {
         for (int i = 0; i < word.Length; i++)
         {
             charsBlack[i].text = word.Substring(i, 1);
+        }
+        */
+
+        wordSun = linesSun[line];
+
+        for (int i = 0; i < charsWhiteSun.Count; i++)
+        {
+            Destroy(charsWhiteSun[i]);
+        }
+
+        charsBlackSun = new List<Text>();
+        charsWhiteSun = new List<Text>();
+
+        posBlackSun = charBlackSun.transform.position;
+        posWhiteSun = charWhiteSun.transform.position;
+
+        for (int i = 0; i < wordSun.Length; i++)
+        {
+            posBlackSun.x += spacing;
+            charsBlackSun.Add(Instantiate(charBlackSun, posBlackSun, Quaternion.identity));
+            charsBlackSun[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordSun.Length; i++)
+        {
+            posWhiteSun.x += spacing;
+            charsWhiteSun.Add(Instantiate(charWhiteSun, posWhiteSun, Quaternion.identity));
+            charsWhiteSun[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordSun.Length; i++)
+        {
+            charsBlackSun[i].text = wordSun.Substring(i, 1);
+        }
+
+        //---------------------------------------------------------
+
+        wordStar = linesStar[line];
+
+        for (int i = 0; i < charsWhiteStar.Count; i++)
+        {
+            Destroy(charsWhiteStar[i]);
+        }
+
+        charsBlackStar = new List<Text>();
+        charsWhiteStar = new List<Text>();
+
+        posBlackStar = charBlackStar.transform.position;
+        posWhiteStar = charWhiteStar.transform.position;
+
+        for (int i = 0; i < wordStar.Length; i++)
+        {
+            posBlackStar.x += spacing;
+            charsBlackStar.Add(Instantiate(charBlackStar, posBlackStar, Quaternion.identity));
+            charsBlackStar[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordStar.Length; i++)
+        {
+            posWhiteStar.x += spacing;
+            charsWhiteStar.Add(Instantiate(charWhiteStar, posWhiteStar, Quaternion.identity));
+            charsWhiteStar[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordStar.Length; i++)
+        {
+            charsBlackStar[i].text = wordStar.Substring(i, 1);
+        }
+
+        //--------------------------------------------------------
+
+        wordBonfire = linesBonfire[line];
+
+        for (int i = 0; i < charsWhiteBonfire.Count; i++)
+        {
+            Destroy(charsWhiteBonfire[i]);
+        }
+
+        charsBlackBonfire = new List<Text>();
+        charsWhiteBonfire = new List<Text>();
+
+        posBlackBonfire = charBlackBonfire.transform.position;
+        posWhiteBonfire = charWhiteBonfire.transform.position;
+
+        for (int i = 0; i < wordBonfire.Length; i++)
+        {
+            posBlackBonfire.x += spacing;
+            charsBlackBonfire.Add(Instantiate(charBlackBonfire, posBlackBonfire, Quaternion.identity));
+            charsBlackBonfire[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordBonfire.Length; i++)
+        {
+            posWhiteBonfire.x += spacing;
+            charsWhiteBonfire.Add(Instantiate(charWhiteBonfire, posWhiteBonfire, Quaternion.identity));
+            charsWhiteBonfire[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordBonfire.Length; i++)
+        {
+            charsBlackBonfire[i].text = wordBonfire.Substring(i, 1);
+        }
+
+        //------------------------------------------------------
+
+        wordCandle = linesCandle[line];
+
+        for (int i = 0; i < charsWhiteCandle.Count; i++)
+        {
+            Destroy(charsWhiteCandle[i]);
+        }
+
+        charsBlackCandle = new List<Text>();
+        charsWhiteCandle = new List<Text>();
+
+        posBlackCandle = charBlackCandle.transform.position;
+        posWhiteCandle = charWhiteCandle.transform.position;
+
+        for (int i = 0; i < wordCandle.Length; i++)
+        {
+            posBlackCandle.x += spacing;
+            charsBlackCandle.Add(Instantiate(charBlackCandle, posBlackCandle, Quaternion.identity));
+            charsBlackCandle[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordCandle.Length; i++)
+        {
+            posWhiteCandle.x += spacing;
+            charsWhiteCandle.Add(Instantiate(charWhiteCandle, posWhiteCandle, Quaternion.identity));
+            charsWhiteCandle[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordCandle.Length; i++)
+        {
+            charsBlackCandle[i].text = wordCandle.Substring(i, 1);
+        }
+    }
+
+    void SetUp()
+    {
+        posBlackSun = charBlackSun.transform.position;
+        posWhiteSun = charWhiteSun.transform.position;
+
+        for (int i = 0; i < wordSun.Length; i++)
+        {
+            posBlackSun.x += spacing;
+            charsBlackSun.Add(Instantiate(charBlackSun, posBlackSun, Quaternion.identity));
+            charsBlackSun[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordSun.Length; i++)
+        {
+            posWhiteSun.x += spacing;
+            charsWhiteSun.Add(Instantiate(charWhiteSun, posWhiteSun, Quaternion.identity));
+            charsWhiteSun[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordSun.Length; i++)
+        {
+            charsBlackSun[i].text = wordSun.Substring(i, 1);
+        }
+
+        //------------------------
+
+        posBlackStar = charBlackStar.transform.position;
+        posWhiteStar = charWhiteStar.transform.position;
+
+        for (int i = 0; i < wordStar.Length; i++)
+        {
+            posBlackStar.x += spacing;
+            charsBlackStar.Add(Instantiate(charBlackStar, posBlackStar, Quaternion.identity));
+            charsBlackStar[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordStar.Length; i++)
+        {
+            posWhiteStar.x += spacing;
+            charsWhiteStar.Add(Instantiate(charWhiteStar, posWhiteStar, Quaternion.identity));
+            charsWhiteStar[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordStar.Length; i++)
+        {
+            charsBlackStar[i].text = wordStar.Substring(i, 1);
+        }
+
+        //------------------------------
+
+        posBlackBonfire = charBlackBonfire.transform.position;
+        posWhiteBonfire = charWhiteBonfire.transform.position;
+
+        for (int i = 0; i < wordBonfire.Length; i++)
+        {
+            posBlackBonfire.x += spacing;
+            charsBlackBonfire.Add(Instantiate(charBlackBonfire, posBlackBonfire, Quaternion.identity));
+            charsBlackBonfire[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordBonfire.Length; i++)
+        {
+            posWhiteBonfire.x += spacing;
+            charsWhiteBonfire.Add(Instantiate(charWhiteBonfire, posWhiteBonfire, Quaternion.identity));
+            charsWhiteBonfire[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordBonfire.Length; i++)
+        {
+            charsBlackBonfire[i].text = wordBonfire.Substring(i, 1);
+        }
+
+        //----------------------------
+
+        posBlackCandle = charBlackCandle.transform.position;
+        posWhiteCandle = charWhiteCandle.transform.position;
+
+        for (int i = 0; i < wordCandle.Length; i++)
+        {
+            posBlackCandle.x += spacing;
+            charsBlackCandle.Add(Instantiate(charBlackCandle, posBlackCandle, Quaternion.identity));
+            charsBlackCandle[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordCandle.Length; i++)
+        {
+            posWhiteCandle.x += spacing;
+            charsWhiteCandle.Add(Instantiate(charWhiteCandle, posWhiteCandle, Quaternion.identity));
+            charsWhiteCandle[i].transform.SetParent(canvasParent.transform);
+        }
+
+        for (int i = 0; i < wordCandle.Length; i++)
+        {
+            charsBlackCandle[i].text = wordCandle.Substring(i, 1);
         }
     }
 }
